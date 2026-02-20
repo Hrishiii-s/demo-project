faf1ff0645535616a3fffc73148c23ef01805215
Hrishiii-s <hrishikesh_s@ecesistech.com>
2026-02-20 17:17:56 +0530

    fix (mobile view nav)

--- components/bpo/bpo_mortgage.jsx
+++ components/bpo/bpo_mortgage.jsx
@@ -1,14 +1,5 @@
 import { motion} from "framer-motion";
-import {
-  DollarSign,
-  Layers,
-  ArrowUpRight,
-  Clock,
-  ShieldCheck,
-  FileText,
-  Home,
-  BarChart3,
-} from "lucide-react";
+import { FileText, Home, BarChart3 } from "lucide-react";
 
 
 
